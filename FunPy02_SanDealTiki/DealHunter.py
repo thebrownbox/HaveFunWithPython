ec326e15c6dea6205ccf5938fa35dcfe29d47a18
thebrownbox <conghv123@gmail.com>
2020-08-24 20:41:24 +0700

    fix thread issue

--- FunPy02_SanDealTiki/DealHunter.py
+++ FunPy02_SanDealTiki/DealHunter.py
@@ -5,16 +5,19 @@ from Item import Item
 
 class DealHunter (Thread):
 
-    def __init__(self, id, name):
-        Thread.__init__(self)
-        self.threadID = id
-        self.name = name
+    # def __init__(self, id, name):
+    #     Thread.__init__(self)
+    #     self.threadID = id
+    #     self.name = name
 
-    def start(self):
-        print("Thread " + self.name + " is started!")
+    # def start(self):
+    #     print("Thread " + self.name + " is started!")
 
     def run(self):
-        count = 1000000
-        while (count > 0):
-            count -= 1
-            print(count)
+        i = 0
+        time.sleep(1)
+        while True:
+            i += 1
+            print(self.name + " - " + str(i))
+            if i > 100:
+                break
--- FunPy02_SanDealTiki/SanDealTiki.py
+++ FunPy02_SanDealTiki/SanDealTiki.py
@@ -17,14 +17,29 @@ listItem = Helper.getItems(INPUT_FILE)
     # DealHunter(item).info()
 
 def main():
-    t1 = DealHunter(1, "AAAAAAAAAAAA")
-    t2 = DealHunter(2, "------------")
+    # t1 = DealHunter(1, "AAAAAAAAAAAA")
+    # t2 = DealHunter(2, "------------")
+
+    t1 = DealHunter()
+    t2 = DealHunter()
 
     t1.start()
-    t1.join()
-    
     t2.start()
+
+    t1.join()
     t2.join()
+
+class MyThread(Thread):
+    def run(self):
+        time.sleep(5)
+        return
+
 
 if __name__ == '__main__':
     main()
+    # for i in range(3):
+    #     t = MyThread()
+    #     t.start()
+    #     print ('t.is_alive()=' + str(t.is_alive()))
+    #     t.join()
+    #     print ('t.is_alive()=' + str(t.is_alive()))
